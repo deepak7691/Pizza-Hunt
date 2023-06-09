b8e3cd94edf5c5786b6d83595049addef40e522c
Deepak Kawliya <kawliya11@gmail.com>
2023-06-09 15:42:52 +0530

    deepak

--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate, } from 'react-router-dom';
+import "../styles/OrderDetails.css"
 
 const OrderDetails = () => {
   const location = useLocation();
